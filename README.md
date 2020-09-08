061258ce1ba1f04ec5dad56cc8af2cdc22268151
letanphuc1214 <tanphuc1214@gmail.com>
2020-09-08 17:17:20 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # module-3
+
+# BT-Xac dinh khoa chinh khoa ngoai
+![Test Image 3](week1/BT-XacDinhKhoaChinhKhoaNgoai/BT-XacDinhKhoaChinhKhoaNgoai.png)
--- week1/BT-XacDinhKhoaChinhKhoaNgoai/BT-XacDinhKhoaChinhKhoaNgoai.sql
+++
@@ -1,5 +0,0 @@
-alter table account add customer_number int;
-alter table account 
-add foreign key (customer_number) references customers (customer_number);
-alter table transaction
-add foreign key (account_number) references account (account_number);
